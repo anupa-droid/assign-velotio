5fcf4ecb4642eb4adfcbf8f36ec7b1446e59e83e
anupa-droid <53728668+anupa-droid@users.noreply.github.com>
2021-04-14 00:35:41 +0530

    Update q2+bonus.py

--- q2+bonus.py
+++ q2+bonus.py
@@ -1,1 +1,15 @@
+import psutil
+import time
 
+while True:
+    cpuperc = psutil.cpu_percent()
+    vmem = psutil.virtual_memory()
+    ram = psutil.virtual_memory().percent
+    availMem = psutil.virtual_memory().available * 100 / psutil.virtual_memory().total
+    
+    print("cpu percentage:" ,cpuperc)
+    print("virtual memory:" ,vmem)
+    print("ram percentage used:", ram )
+    print("mem available:" , availMem)
+    
+    time.sleep(600.00)
